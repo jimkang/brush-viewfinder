9b5973cd06982283865b594f745717b0152dc97e
Jim Kang <jimkang@gmail.com>
2015-09-04 21:08:43 -0400

    Updated to reuse existing elements on rerenders. Axis ticks and brush size are currently not updating correctly on rerenders.

--- brushies.js
+++ brushies.js
@@ -3,78 +3,84 @@ var d3Scale = require('d3-scale');
 var d3Random = require('d3-random')
 var d3Svg = require('./lib/d3-small').svg;
 
-function renderBrushies(viewfinder, syncElementsToView) {
-  // var data = d3Arrays.range(800).map(Math.random);
+var margin = {
+  top: 194,
+  right: 50,
+  bottom: 214,
+  left: 50
+};
 
-  var margin = {top: 194, right: 50, bottom: 214, left: 50},
-      width = 960 - margin.left - margin.right,
-      height = 500 - margin.top - margin.bottom;
+var width = 960 - margin.left - margin.right;
+var height = 500 - margin.top - margin.bottom;
+var brush;
+var svg;
+var axisGroup;
+var brushg;
+var resizerLabels;
+
+var arc = d3Svg.arc()
+    .outerRadius(height / 2)
+    .startAngle(0)
+    .endAngle(function(d, i) { return i ? -Math.PI : Math.PI; });
+
+function renderBrushies(viewfinder, syncElementsToView) {
+  var array = viewfinder.getWholeArray();
 
   var x = d3Scale.linear()
-    .domain([0, viewfinder.getWholeArray().length])
+    .domain([0, array.length])
     .range([0, width]);
 
-  // var y = d3Random.normal(height / 2, height / 8);
-
-  var brush = d3Svg.brush()
+  if (!brush) {
+    brush = d3Svg.brush()
       .x(x)
       .extent([.3, .5])
       .on('brushstart', brushstart)
       .on('brush', brushmove)
       .on('brushend', brushend);
+  }
 
-  var arc = d3Svg.arc()
-      .outerRadius(height / 2)
-      .startAngle(0)
-      .endAngle(function(d, i) { return i ? -Math.PI : Math.PI; });
-
-  var svg = d3.select('#brush-root').append('svg')
+  if (!svg) {
+    svg = d3.select('#brush-root').append('svg')
       .attr('width', width + margin.left + margin.right)
       .attr('height', height + margin.top + margin.bottom)
     .append('g')
       .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-  svg.append('g')
+    brushg = svg.append('g')
+        .attr('class', 'brush')
+        .call(brush);
+
+    var resizers = brushg.selectAll('.resize').append('g')
+        .attr('transform', 'translate(0,' +  height / 2 + ')');
+
+    resizers.append('path').attr('d', arc);
+
+    resizerLabels = resizers.append('text');
+    
+    brushg.selectAll('rect')
+        .attr('height', height);
+
+  }
+  else {
+    brushg.call(brush);
+  }
+
+  updateBrush();
+
+  if (!axisGroup) {
+    axisGroup = svg.append('g')
       .attr('class', 'x axis')
-      .attr('transform', 'translate(0,' + height + ')')
-      .call(d3Svg.axis().scale(x).orient('bottom'));
-
-  // var circle = svg.append('g').selectAll('circle')
-  //     .data(data)
-  //   .enter().append('circle')
-  //     .attr('transform', function(d) { return 'translate(' + x(d) + ',' + y() + ')'; })
-  //     .attr('r', 3.5);
-
-  var brushg = svg.append('g')
-      .attr('class', 'brush')
-      .call(brush);
-
-  var resizers = brushg.selectAll('.resize').append('g')
-      .attr('transform', 'translate(0,' +  height / 2 + ')');
-
-  resizers.append('path')
-      .attr('d', arc);
-
-  var resizerLabels = resizers.append('text');
-  // .text('label');
-  
-  brushg.selectAll('rect')
-      .attr('height', height);
-
-  brushstart();
-  brushmove();
+      .attr('transform', 'translate(0,' + height + ')');
+  }
+  axisGroup.call(d3Svg.axis().scale(x).orient('bottom'));
 
   function brushstart() {
     svg.classed('selecting', true);
-    updateBrushLabels();
+    updateBrush();
   }
 
   function brushmove() {
-    var extent = brush.extent();    
-    viewfinder.shift(extent[0] - viewfinder.getIndex());
-    viewfinder.resizeView(extent[1] - extent[0]);
-
-    updateBrushLabels();
+    updateBrush();
     syncElementsToView();
     // circle.classed('selected', function(d) { return s[0] <= d && d <= s[1]; });
   }
@@ -83,7 +89,10 @@ function renderBrushies(viewfinder, syncElementsToView) {
     svg.classed('selecting', !d3.event.target.empty());
   }
 
-  function updateBrushLabels() {
+  function updateBrush() {
+    var extent = brush.extent();    
+    viewfinder.shift(extent[0] - viewfinder.getIndex());
+    viewfinder.resizeView(extent[1] - extent[0]);
     resizerLabels.text(getLabelTextForBrushData);
   }
 
